bfcb708cbf957675c51737ec3a57302e675054ee
Johannes <Joapfel@Michaela-Lutz.local>
2016-11-16 17:42:31 +0100

    Graph implementation

--- src/de/ws1617/pccl/search/Graph.java
+++ src/de/ws1617/pccl/search/Graph.java
@@ -1,9 +1,13 @@
 package de.ws1617.pccl.search;
 
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import de.ws1617.pccl.fsa.Edge;
@@ -11,7 +15,9 @@ import de.ws1617.pccl.grammar.Terminal;
 
 public class Graph {
 
-	// TODO add instance fields here 
+	private ArrayList<HashSet<Edge>> adj;
+	
+	private boolean[] finalStates;
 	
 	/**
 	 * Initialize the adjacency and final state array.
@@ -20,21 +26,31 @@ public class Graph {
 	 *            the number of vertices in the graph.
 	 */
 	public Graph(int v) {
-		// TODO implement me !
+		
+		ArrayList<HashSet> adj = new ArrayList<HashSet>();
+		boolean[] finalStates = new boolean[v];
+		
 	}
 
+	/**
+	 * Add the edge to the ArrayList.
+	 * @param from - current node of the ArrayList
+	 * @param edge - new node which will be added to the current node
+	 */
 	public void addEdge(int from, Edge edge) {
-		// TODO implement me !
+		
+		HashSet<Edge> hs = adj.get(from);
+		hs.add(edge);
+		
 	}
 
 	public HashSet<Edge> getAdjacent(int from) {
 
-		// TODO implement me !
-		return null;
+		return adj.get(from);
 	}
 
 	/**
-	 * Returns all edges that point from a certain state to adjacent states and can be reached when consuimg the given {@link Terminal}.
+	 * Returns all edges that point from a certain state to adjacent states and can be reached when consuming the given {@link Terminal}.
 	 * 
 	 * @param from the current state.
 	 * @param toConsume the next terminal to consume.
@@ -42,8 +58,16 @@ public class Graph {
 	 */
 	public HashSet<Edge> getAdjacent(int from, Terminal toConsume) {
 	
-		// TODO implement me !
-		return null;
+		HashSet<Edge> result = new HashSet<>();
+		
+		for(Edge e : adj.get(from)){
+			
+			if(e.getToConsume().getValue().equals(toConsume)){
+				result.add(e);
+			}
+			
+		}
+		return result;
 	}
 
 	/**
