e4b76ba1e3b98f2d9da1d3b3809f2effc50d00e0
Johannes <kraemer_johannes@gmx.de>
2016-11-21 18:06:44 +0100

    Main method is implemented

--- src/de/ws1617/pccl/app/Main.java
+++ src/de/ws1617/pccl/app/Main.java
@@ -6,24 +6,45 @@ import de.ws1617.pccl.grammar.Grammar;
 import de.ws1617.pccl.grammar.GrammarUtils;
 import de.ws1617.pccl.grammar.Lexicon;
 import de.ws1617.pccl.grammar.NonTerminal;
+import de.ws1617.pccl.search.Automaton;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		
 		// read runtime arguments
 		try {
+			// TODO create grammar, lexicon, start symbol
 			Grammar grammar = GrammarUtils.readGrammar(args[0]);
 			Lexicon lexicon = GrammarUtils.readLexicon(args[1]);
 			NonTerminal startSymbol = new NonTerminal(args[2]);
 			String input = args[3];
 
-			// TODO create grammar, lexicon, start symbol
-
 			// TODO create an Automaton object
-
+			Automaton automat = new Automaton(grammar, lexicon, startSymbol);
+			
 			// TODO print out whether the given input is in the language
-
+			if(automat.recognize(input)){
+				System.out.println("Processed by: ");
+				System.out.println("[Non-deterministic finite automaton]");
+				System.out.println();
+				System.out.println("User input string: ");
+				System.out.println("[" + input + "]");
+				System.out.println();
+				System.out.println("Accepted: ");
+				System.out.println("[yes]");
+			}else{
+				System.out.println("Processed by: ");
+				System.out.println("[Non-deterministic finite automaton]");
+				System.out.println();
+				System.out.println("User input string: ");
+				System.out.println("[" + input + "]");
+				System.out.println();
+				System.out.println("Accepted: ");
+				System.out.println("[no]");
+			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
